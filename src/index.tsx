82ebe00b4e4a1a9f3e87bb41283e225231bca37e
Abhay Goswami <abhaygoswami676@gmail.com>
2021-09-22 02:16:33 +0530

    remove CRA boilerplate and add TailwindCSS and ChakraUI

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ChakraProvider, extendTheme } from '@chakra-ui/react';
+import '@fontsource/raleway/400.css';
+import '@fontsource/open-sans/700.css';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const theme = extendTheme({
+  fonts: {
+    heading: 'Open Sans',
+    body: 'inter',
+  },
+});
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider theme={theme}>
+      <App />
+    </ChakraProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
